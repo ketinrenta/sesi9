97b1b61b517159d4df59b687b52367afdec4e716
ketinrenta <127809278+ketinrenta@users.noreply.github.com>
2023-05-09 12:09:45 +0800

    Add files via upload

---
+++ javascript04.js
@@ -0,0 +1,10 @@
+function tulis(){
+document.write("contoh text menggunakan javascript");
+document.write("<br>menjadi dan memberi");
+}
+
+function checknama(){
+    let nm = document.getElementById("txNAMA").value;
+
+    console.log("isi dari nm:"+nm);
+}
